86bf7d936e5be5287c8aa43f016290b3d171e036
KlaasVanDerLinden <vanderlinden@culturalservices.nl>
2017-04-26 13:11:39 +0200

    Update jquery.ui.monthpicker.js
    fixed the custom jquery.effects ($.effects.effect)

--- jquery.ui.monthpicker.js
+++ jquery.ui.monthpicker.js
@@ -297,7 +297,7 @@
 			inst.dpDiv.zIndex($(input).zIndex()+1);
 			$.monthpicker._monthpickerShowing = true;
 
-			if ($.effects && $.effects[showAnim])
+			if ($.effects && $.effects.effect[showAnim])
 				inst.dpDiv.show(showAnim, $.monthpicker._get(inst, 'showOptions'), duration, postProcess);
 			else
 				inst.dpDiv[showAnim || 'show']((showAnim ? duration : null), postProcess);
@@ -362,7 +362,7 @@
 					this._curInst = null;
 				};
 
-				if ( $.effects && $.effects[ showAnim ] )
+				if ( $.effects && $.effects.effect[ showAnim ] )
 					inst.dpDiv.hide(showAnim, $.monthpicker._get(inst, 'showOptions'), duration, postProcess);
 				else
 					inst.dpDiv[(showAnim == 'slideDown' ? 'slideUp' :
